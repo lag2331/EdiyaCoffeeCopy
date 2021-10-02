e7722a0e61801d3e15741df10032f8ae9d1d85e4
SUNYEONG SO <lag2331@gmail.com>
2021-10-02 13:07:54 +0900

    Add *

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+     $('.img_box').bxSlider({
+        auto: true,
+        speed: 500,
+        pause: 4000,
+        pager:false,
+        controls: false,
+        infiniteLoop: true,
+    }); 
+});
