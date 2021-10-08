a3b4864559aeaa3a4f1a052ad5593d5396a1867f
SUNYEONG SO <lag2331@gmail.com>
2021-10-08 17:38:56 +0900

    Add *

--- js/main.js
+++ js/main.js
@@ -1,10 +1,61 @@
 $(document).ready(function(){
-     $('.img_box').bxSlider({
+     $('.visual_slide').bxSlider({
         auto: true,
-        speed: 500,
-        pause: 4000,
+        speed: 1500,
+        pause: 3500,
         pager:false,
         controls: false,
         infiniteLoop: true,
-    }); 
+    });
+
+    $('.beanist_slide').bxSlider({
+        auto: true,
+        speed: 1000,
+        pause: 3000,
+        pager:false,
+        controls: false,
+        infiniteLoop: true,
+    });
+
+    // $('.top_nav2').click(function(){
+    //     $('.top_nav2_sub').css(
+    //         "opacity","1",
+    //         "visibility","visible",
+    //         "z-index","9999"
+    //     );
+    // });
+
+    // $(".in_up").on("mouseover", function(){
+    //     $("#testDiv1").fadeIn();
+    // });
+
+    var isVisible = false;
+
+    $(window).on('scroll',function() {
+        if (checkVisible($('.in_up'))&&!isVisible) {
+            $("#in_up").fadeOut();
+            isVisible=true;
+        }
+    });
+
+    function checkVisible( elm, eval ) {
+        eval = eval || "object visible";
+        var viewportHeight = $(window).height(), // Viewport Height
+            scrolltop = $(window).scrollTop(), // Scroll Top
+            y = $(elm).offset().top,
+            elementHeight = $(elm).height();   
+        
+        if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
+        if (eval == "above") return ((y < (viewportHeight + scrolltop)));
+    }
+
+    $(document).on('mouseover', '.drop_menu span', function() {
+        $('.sub').slideDown(300);
+    });
+    $(document).on('mouseover', 'div', function () {
+        if (!$(this).hasClass('drop_menu')) {
+            $('.sub').slideUp(300);
+        }
+    });
+
 });
