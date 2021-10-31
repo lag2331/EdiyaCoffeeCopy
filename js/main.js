778415976af6be429cdaa1cd858562163710fa11
lag2331 <lag2331@gmail.com>
2021-11-01 00:51:18 +0900

    scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 $(document).ready(function(){
+
+    console.log($(".in_up").scrollTop());
+
+    const navbarHeight = $('.top_nav').height();
+    const in_upScrollTop = $(".in_up").scrollTop();
+    $(window).scroll(function(){
+        if(window.scrollY > navbarHeight) {
+            $('.top_nav').addClass('top_nav-dark');
+        }else{
+            $('.top_nav').removeClass('top_nav-dark');
+        }
+
+        if(window.scrollY > 2500 ){
+            console.log($('.main_block2_con'));
+            console.log(window.scrollY);
+            console.log(in_upScrollTop);
+            $(".in_up").addClass('appear');
+        }
+    });
+
      $('.visual_slide').bxSlider({
         auto: true,
         speed: 1500,
@@ -16,38 +36,6 @@ $(document).ready(function(){
         controls: false,
         infiniteLoop: true,
     });
-
-    // $('.top_nav2').click(function(){
-    //     $('.top_nav2_sub').css(
-    //         "opacity","1",
-    //         "visibility","visible",
-    //         "z-index","9999"
-    //     );
-    // });
-
-    // $(".in_up").on("mouseover", function(){
-    //     $("#testDiv1").fadeIn();
-    // });
-
-    var isVisible = false;
-
-    $(window).on('scroll',function() {
-        if (checkVisible($('.in_up'))&&!isVisible) {
-            $("#in_up").fadeOut();
-            isVisible=true;
-        }
-    });
-
-    function checkVisible( elm, eval ) {
-        eval = eval || "object visible";
-        var viewportHeight = $(window).height(), // Viewport Height
-            scrolltop = $(window).scrollTop(), // Scroll Top
-            y = $(elm).offset().top,
-            elementHeight = $(elm).height();   
-        
-        if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
-        if (eval == "above") return ((y < (viewportHeight + scrolltop)));
-    }
 
     $(document).on('mouseover', '.drop_menu span', function() {
         $('.sub').slideDown(300);
